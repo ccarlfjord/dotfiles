13af64679e3c270ee3bd0ac5c0ed16096e439db5
Charles Carlfjord <charles@gauz.se>
2023-07-14 22:48:36 +0200

    nvim: keymaps

--- .config/nvim/lua/gauz/keymap.lua
+++ .config/nvim/lua/gauz/keymap.lua
@@ -5,6 +5,10 @@ vim.keymap.set('n', '<Space>', '<Nop>', {silent = true, remap = false })
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>s", vim.lsp.buf.format)
+vim.keymap.set({'n', 'v'}, "<leader>i", '<C-I>')
+vim.keymap.set({'n', 'v'}, "<leader>o", '<C-O>')
+
 
 -- Copy to clipboard
 vim.keymap.set('v', '<leader>y', '+y')
