c724b0fcd43d5d6485dbaa905458eba22cfe86f1
Charles Carlfjord <charles.carlfjord@tink.com>
2023-11-13 11:28:57 +0100

    fix(tms): shellcheck

--- tms.sh
+++ tms.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 main() {
-	local dir=$(find ~/src -mindepth 1 -maxdepth 1 -type d| fzf)
+	local dir=$(find ~/src ~/src/tink -mindepth 1 -maxdepth 1 -type d | fzf)
 
 	if [[ -z $dir ]]; then
     	exit 0
 	fi
-	
-	name=$(basename $dir)
+
+	name=$(basename "${dir}")
 	running=$(pgrep tmux)
-	
 	if [[ -z $TMUX ]] && [[ -z $running ]]; then
-		tmux new -s $name -c $dir
+		tmux new -s "${name}" -c "${dir}"
 		exit 0
 	fi
 
-	if ! tmux has-session -t $name 2>/dev/null; then
-		tmux new -ds $name -c $dir
+	if ! tmux has-session -t "${name}" 2>/dev/null; then
+		tmux new -ds "${name}" -c "${dir}"
 	fi
 
 	if [[ -z $TMUX ]]; then
-		tmux attach -t $name
+		tmux attach -t "${name}"
 		exit 0
 	fi
 
-	tmux switchc -t $name
+	tmux switchc -t "${name}"
 }
 
 main
