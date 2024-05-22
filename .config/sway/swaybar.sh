7d512e4c4cc468ada4615b22276730dddba18e25
Charles Carlfjord <charles.carlfjord@voiapp.io>
2024-05-22 21:09:08 +0200

    feat(sway): various config changes
    * use waybar
    * kanshi with laptop profiles
    * sway hotkeys

--- .config/sway/swaybar.sh
+++ .config/sway/swaybar.sh
@@ -3,11 +3,15 @@
 battery_status=''
 
 if [ -e /sys/class/power_supply/BAT0/status ]; then
-	battery_status="󰁹 $(cat /sys/class/power_supply/BAT0/status) "
+	bat_now=$(cat /sys/class/power_supply/BAT0/energy_now)
+	bat_full=$(cat /sys/class/power_supply/BAT0/energy_full)
+	bat_percentage=$(bc <<< "100 * ${bat_now}/${bat_full}")
+	battery_status="󰁹 ${bat_percentage}% - $(cat /sys/class/power_supply/BAT0/status) "
 fi
 
-date_formatted=" $(date +'%a %H:%M:%S %d/%m/%Y') "
+
+date_formatted=" $(date +'%H:%M:%S %a %d/%m/%Y ') "
 active_keyboard_layout=" $(swaymsg -t get_inputs | jq -r '.[0].xkb_active_layout_name') "
-wifi_info="  $(nmcli -g NAME connection show  --active | grep -v lo || "N/A") "
+wifi_info="  $(nmcli -g NAME connection show  --active | grep -v lo -v docker0 || "N/A") "
 
 echo "$date_formatted$battery_status$wifi_info$active_keyboard_layout"
