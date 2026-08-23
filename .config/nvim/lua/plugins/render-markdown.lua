b65749199a5e092b0afade6dc4abd090a626f337
Charles Carlfjord <charles@gauz.se>
2026-08-23 15:56:08 +0200

    chore: update telescope and treesitter

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -4,6 +4,16 @@ return {
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
-		opts = {},
+		opts = {
+			file_types = { 'markdown' },
+			completions = {
+				lsp = { enabled = false },
+			},
+			overrides = {
+				buftype = {
+					nofile = { enabled = false },
+				},
+			},
+		},
 	}
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,64 +2,67 @@ local setupTelescope = function()
 
 end
 return {
-	"ANGkeith/telescope-terraform-doc.nvim",
-	'nvim-lua/plenary.nvim',
-	"nvim-telescope/telescope-file-browser.nvim",
-	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
-	},
 	{
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.8',
-		init = function()
-		end,
-		opts = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-			vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<CR>', {})
-			vim.keymap.set('n', '<leader>fs', builtin.lsp_dynamic_workspace_symbols, {})
-
-			require('telescope').load_extension('file_browser')
-			require('telescope').load_extension('terraform_doc')
-
-			local telescopeConfig = require("telescope.config")
-			local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
-			-- Find in dotfiles
-			table.insert(vimgrep_arguments, "--hidden")
-			-- I don't want to search in the `.git` directory.
-			table.insert(vimgrep_arguments, "--glob")
-			table.insert(vimgrep_arguments, "!**/.git/*")
-
+		branch = 'master',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'ANGkeith/telescope-terraform-doc.nvim',
+			'nvim-telescope/telescope-file-browser.nvim',
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+			},
+		},
+		keys = {
+			{ '<leader>ff', '<cmd>Telescope find_files<cr>',                    desc = 'Find files' },
+			{ '<leader>fg', '<cmd>Telescope live_grep<cr>',                     desc = 'Live grep' },
+			{ '<leader>fb', '<cmd>Telescope buffers<cr>',                       desc = 'Buffers' },
+			{ '<leader>fh', '<cmd>Telescope help_tags<cr>',                     desc = 'Help tags' },
+			{ '<leader>fd', '<cmd>Telescope diagnostics<cr>',                   desc = 'Diagnostics' },
+			{ '<leader>fs', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>', desc = 'LSP dynamic workspace symbols' },
+		},
+		opts = {
+			defaults = {
+				vimgrep_arguments = {
+					'rg',
+					'--color=never',
+					'--no-heading',
+					'--with-filename',
+					'--line-number',
+					'--column',
+					'--smart-case',
+					'--hidden',
+					'--glob',
+					'!**/.git/*',
+				},
+				file_ignore_patterns = {
+					'vendor',
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+				},
+			},
+			extensions = {
+				file_browser = {
+					hijack_netrw = false,
+					hidden = true,
+				},
+			},
+		},
+		config = function(_, opts)
 			local telescope = require('telescope')
+			telescope.setup(opts)
 
-			return {
-				defaults = {
-					vimgrep_arguments = vimgrep_arguments,
-					file_ignore_patterns = {
-						"vendor",
-					}
-
-				},
-				pickers = {
-					find_files = {
-						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-					},
-				},
-				extensions = {
-					file_browser = {
-						hijack_netrw = false,
-						hidden = true,
-					},
-				},
-			}
-		end
+			-- Load extensions safely after setup
+			telescope.load_extension('file_browser')
+			telescope.load_extension('terraform_doc')
+			pcall(telescope.load_extension, 'fzf')
+		end,
 	},
 }
-
 
 -- local builtin = require('telescope.builtin')
 -- -- vim.keymap.set('n', '<leader>fe', '<cmd>Telescope file_browser<CR>', {})
